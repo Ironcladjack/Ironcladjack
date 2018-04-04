4538fb9cf5cdeeef5137e5cf9bd0bd541691e576
Ironcladjack <Ironcladjack@users.noreply.github.com>
2018-04-04 18:01:07 +0100

    Finished Tutorial
    Improved CSS

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 
 // This function loads in HTML from other files, just put a '<div data-include="<filename>"></div>'
 $(document).ready(function() {
+
   var includes = $('[data-include]');
   jQuery.each(includes, function(){
-    var file = 'components/' + $(this).data('include') + '.html';
-    $(this).load(file);
+    var html = 'components/' + $(this).data('include') + '.html';
+    var css = 'components/' + $(this).data('include') + '.css';
+    $(this).load(css).load(html);
   });
+
 });
